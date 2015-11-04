edce63f6184edecb584f5040026042ff001406a0
Adam Cooper <adam.cooper@nottingham.ac.uk>
2015-11-04 11:28:29 +0000

    Functionality now in place to allow upgrades to the plugin.
    This allows us to push new elements without affecting existing ones.

--- MOASElementsPlugin.php
+++ MOASElementsPlugin.php
@@ -13,7 +13,8 @@ class MOASElementsPlugin extends Omeka_Plugin_AbstractPlugin
     protected $_hooks = array(
         'install',
         'uninstall',
-        'uninstall_message'
+        'uninstall_message',
+        'upgrade'
     );
 
     public function __construct()
@@ -31,7 +32,6 @@ class MOASElementsPlugin extends Omeka_Plugin_AbstractPlugin
      */
     public function hookInstall()
     {
-        // global function (ick) that adds our metadata set.
         insert_element_set($this->_elementSetMetadata, $this->_elements);
     }
 
@@ -58,5 +58,44 @@ class MOASElementsPlugin extends Omeka_Plugin_AbstractPlugin
         echo __('%sWarning%s: This will remove all the %s elements added '
             . 'by this plugin and permanently delete all element texts entered in those '
             . 'fields.%s', '<p><strong>', '</strong>', $this->_elementSetMetadata['name'], '</p>');
+    }
+
+    public function hookUpgrade($oldVersion, $newVersion)
+    {
+        switch($oldVersion)
+        {
+            // let the plugin cascade its upgrades
+            case '1.0.0':
+                // code to upgrade from 1.0.0 to 1.1.0
+            default :
+                $this->_updateElements();
+        }
+    }
+
+    /**
+     * Adds any elements that have been defined but are not present on the site.
+     */
+    private function _updateElements($remove = false)
+    {
+        // get database records.
+        /** @var Table_ElementSet $elementSetTable */
+        $elementSetTable = $this->_db->getTable('ElementSet');
+        /** @var ElementSet $elementSet */
+        $elementSet = $elementSetTable->findByName($this->_elementSetMetadata['name']);
+        $elements = $elementSet->getElements();
+
+        // walk the MOAS element array and add where needed.
+        array_walk($this->_elements, function($element, $index) use (&$elementSet, $elements) {
+            foreach ($elements as $dbElement) {
+                if ($element['name'] === $dbElement->name) {
+                    return;
+                }
+            }
+
+            // if we get here it means we didn't find this element in the database.
+            $elementSet->addElements(array($index => $element));
+        });
+
+        $elementSet->save();
     }
 }
--- elements.php
+++ elements.php
@@ -2,12 +2,20 @@
 
 $elementSetMetadata = array(
     'name'        => "MOAS Elements",
-    'description' => "Elements required for MOAS functionality"
+    'description' => "Elements required for MOAS functionality",
+    'record_type'   => "Item"
 );
 $elements = array(
     array(
-        'name'           => "Geo Location",
-        'description'    => "The latitude/longitude pair that allows you to specify an items location.",
-        'record_type'    => "Item"
+        'name'          => "Artist/s",
+        'description'   => "The artist/s involved in the creation of the item. This is used to provide 'More by this artist' functionality",
+    ),
+    array(
+        'name'          => "Geo Location",
+        'description'   => "The Latitude and Longitude pair related to the item, comma separated. E.g. '53.2342, 0.2343'"
+    ),
+    array(
+        'name'          => "Related Item Tag",
+        'description'   => "This allows MOAS to provide related items in appropriate places by showing items tagged in a similar fashion."
     )
 );
