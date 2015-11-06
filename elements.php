aa3fabe06bb02c028b440acda853a4c177e2d220
Adam Cooper <adam.cooper@nottingham.ac.uk>
2015-11-06 11:54:02 +0000

    Add additional metadata for display

--- elements.php
+++ elements.php
@@ -7,12 +7,20 @@ $elementSetMetadata = array(
 );
 $elements = array(
     array(
-        'name'          => "Artist/s",
-        'description'   => "The artist/s involved in the creation of the item. This is used to provide 'More by this artist' functionality",
+        'name'          => "Alternative Item Tag",
+        'description'   => "This allows MOAS to provide related items in appropriate places by showing items tagged in a similar fashion."
+    ),
+    array(
+        'name'          => "Artist",
+        'description'   => "The artist involved in the creation of the item. This is used to provide 'More by this artist' functionality",
     ),
     array(
         'name'          => "Geo Location",
         'description'   => "The Latitude and Longitude pair related to the item, comma separated. E.g. '53.2342, 0.2343'"
+    ),
+    array(
+        'name'          => "Organisation",
+        'description'   => "The organisation responsible for this item."
     ),
     array(
         'name'          => "Related Item Tag",
