8a1760719ae0fd934038f96be74745489033986f
Alan Shortis <alan.shortis@mac.com>
2016-09-23 10:12:27 +0100

    Added country field

--- elements.php
+++ elements.php
@@ -17,44 +17,49 @@ $elements = array(
         'order'         => 3
     ),
     array(
+        'name'          => "Country",
+        'description'   => "The country of origin for this item.",
+        'order'         => 4
+    ),
+    array(
         'name'          => "Geo Location",
         'description'   => "The Latitude and Longitude pair related to the item, comma separated. E.g. '53.2342, 0.2343'",
-        'order'         => 4
+        'order'         => 5
     ),
     array(
         'name'          => "Map Zoom Level",
         'description'   => "The zoom level for the item map, where '1' is global, '10' would show a large city and '18' is street level.",
-        'order'         => 5
+        'order'         => 6
     ),
     array(
         'name'          => "Organisation",
         'description'   => "The organisation responsible for this item.",
-        'order'         => 6
+        'order'         => 7
     ),
     array(
         'name'          => "Related Item Tag",
         'description'   => "This allows MOAS to provide related items in appropriate places by showing items tagged in a similar fashion.",
-        'order'         => 7
+        'order'         => 8
     ),
     array(
         'name'          => "Theme",
         'description'   => "This allows MOAS to provide related items in appropriate places by showing items tagged in a similar fashion.",
-        'order'         => 8
+        'order'         => 9
     ),
     array(
         'name'          => "Download Rights",
         'description'   => "The download rights for this item.",
-        'order'         => 9
+        'order'         => 10
     ),
     array(
         'name'          => "Download Contact",
         'description'   => "An email address or URL for the item owner.",
-        'order'         => 10
+        'order'         => 11
     ),
     array(
         'name'          => "Funding Body",
         'description'   => "The organisation that funded this item.",
-        'order'         => 11,
+        'order'         => 12
     ),
     array(
         'name'          => "URL Slug",
